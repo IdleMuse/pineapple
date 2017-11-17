9b59989017ddfffe22775bfaad644165801ef230
Justin Dee <idlemuse@ghostlit.co.uk>
2017-11-17 15:00:12 +0000

    Lots of hacky fixes

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,22 @@
 module.exports = function(app){
 
-    app.get('/api/posts', function(req, res){
-        res.send('hi');
+    function database_connect(){
+        var mysql = require('mysql')
+        var connection = mysql.createConnection({
+            host     : 'bwdbinstance-cluster.cluster-cidljrammxel.eu-west-1.rds.amazonaws.com',
+            user     : 'hckthn_team2',
+            password : '3917906429',
+            database : 'hckthn_team2_pineapple'
+        });
+
+        return connection;
+        connection.connect()
+    }
+
+    app.get('/api/test', function(req, res){
+        var connection = database_connect();
+        res.send(connection.threadId);
+        connection.end();
     });
 
     app.get('*', function(req, res) {
